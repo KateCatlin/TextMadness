9cf63f4e82a192c68414fd4618cc0bbe0ce96aeb
Planxty-Dan <dan.newport@detroitlabs.com>
2014-10-21 16:34:43 -0400

    add variables for both mainActivity buttons, add empty onClick for each button in onCreate, add three methods (one for spliting the editText field string and adding it to an arraylist, one for adding the random generated word to that arrayList, and one for building the sentence back from the arrayList

--- app/src/main/java/com/bunniesarecute/admin/textmadness/MainActivity.java
+++ app/src/main/java/com/bunniesarecute/admin/textmadness/MainActivity.java
@@ -4,14 +4,59 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends Activity {
+
+    Button generateWordButton;
+    Button shareTextButton;
+    EditText mainEditText;
+    ArrayList<String> editTextStrings = new ArrayList<String>();
+
+    public void addTextToStringArrayList(String text) {
+        int i;
+        String[] splitText = text.split(" ");
+        for (i = 0; i < splitText.length; i++) {
+            editTextStrings.add(splitText[i]);
+        }
+    }
+
+    public void addRandomWordToArrayList(String word) {
+        editTextStrings.add(word);
+    }
+//String builder
+    public String buildText() {
+        String completedTextMessage = "";
+        for(String current: editTextStrings) {
+            completedTextMessage = (completedTextMessage + " " + current);
+        }
+        return completedTextMessage;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainEditText = (EditText) findViewById(R.id.edit_text);
+        generateWordButton = (Button) findViewById(R.id.generate_word_button);
+        generateWordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addTextToStringArrayList(mainEditText.getText().toString());
+            }
+        });
+        shareTextButton = (Button) findViewById(R.id.send);
+        shareTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
 
