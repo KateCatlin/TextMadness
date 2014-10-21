a5d8ec928d1851aed3a764243d83eb03e6b25cc9
Brandy Foster <brandy.foster@detroitlabs.com>
2014-10-21 16:17:56 -0400

    added buttons to on create with click listner.

--- app/src/main/java/com/bunniesarecute/admin/textmadness/ShareOptions.java
+++ app/src/main/java/com/bunniesarecute/admin/textmadness/ShareOptions.java
@@ -4,16 +4,30 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 
-public class ShareOptions extends Activity {
+public class ShareOptions extends Activity implements OnClickListener {
 
-
+    private ImageButton mTextMessageButton;
+    private ImageButton mEmailMessageButton;
+    private ImageButton mFacebookButton;
+    private ImageButton mTwitterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.share_options);
+        mTextMessageButton = (ImageButton) findViewById(R.id.text_message_button);
+        mTextMessageButton.setOnClickListener(this);
+        mEmailMessageButton = (ImageButton) findViewById(R.id.email_button);
+        mEmailMessageButton.setOnClickListener(this);
+        mFacebookButton = (ImageButton) findViewById(R.id.facebook_button);
+        mFacebookButton.setOnClickListener(this);
+        mTwitterButton = (ImageButton) findViewById(R.id.twitter_button);
+        mTwitterButton.setOnClickListener(this);
     }
 
 
@@ -34,5 +48,20 @@ public class ShareOptions extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.text_message_button:
+
+            case R.id.email_button:
+
+            case R.id.facebook_button:
+
+            case R.id.twitter_button:
+        }
+
     }
 }
